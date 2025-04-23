813a11fe363529959912e0b889dc06381ed0f44f
joji-fuck <logvmaks@gmail.com>
2025-04-23 14:27:09 +0500

    1-5 практики

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::paginate(2);
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required',
+        ]);
+        Category::create($request->all());
+       // $request->session()->flash('success',' Категория добавлена');
+        return redirect()->route('categories.index')->with('success',' Категория добавлена');
+        session()->flash('success', 'Запись успешно добавлена!'); 
+        session()->flash('warning', 'Обратите внимание на эту важную информацию!');
+        session()->flash('info', 'Это просто информационное сообщение.'); 
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        dd(__METHOD__);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        $category->update($request->all());
+
+        return redirect()->route('admin.categories.index')->with('success', 'Категория обновлена!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect()->route('admin.categories.index')->with('success', 'Категория удалена!');
+    }
+}
---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.layout')
+
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Создание категории</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Blank Page</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Создание категории</h3>
+                    </div>
+                    <form role="form" method="POST" action="{{ route('admin.categories.store') }}">
+                        @csrf  
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="name">Название</label> 
+                                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ $categoru->title }}" placeholder="Название">
+                                
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Сохранить</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
+
